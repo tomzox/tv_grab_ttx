b18d80943eb8756d1bc805c42f84d2de97cd2900
Th. Zoerner <tomzox@gmail.com>
2008-03-08 13:19:20 +0100

    cap.pl switched to use Video::ZVBI, release 0.2
    cap.pl: revision 1.5
    date: 2008/03/08 13:19:20;  author: tom;  state: Exp;  lines: +83 -73
    Adapted for interface changes to the ZVBI perl module
    - for first official release of Video::ZVBI version 0.2.x

--- cap.pl
+++ cap.pl
@@ -1,10 +1,11 @@
 #!/usr/bin/perl
 #
 #  This script grabs VBI data from a TV card and appends each packet to a
-#  file.  Grabbing VBI requires the V4l module from CPAN.org.  This script
-#  is intended for use with my teletext grabber only. Note that the output
-#  format is preliminary and will definitly change (e.g. to include parity
-#  decoding error counts and VPS.)
+#  file.  VBI acquisition is done via the Video-ZVBI module from CPAN.org
+#  (which in turn depends on the libzvbi library.) This script is intended
+#  for use with the teletext grabber only. Note that the output format is
+#  preliminary and will probably change (e.g. to include parity decoding
+#  error counts and VPS.)
 #
 #
 #  This program is free software; you can redistribute it and/or modify
@@ -19,12 +20,12 @@
 #
 #  Author: Tom Zoerner (tomzo at users.sf.net)
 #
-#  $Id: cap.pl,v 1.4 2006/10/15 19:55:47 tom Exp tom $
+#  $Id: cap.pl,v 1.5 2008/03/08 13:19:20 tom Exp $
 #
 
-#use blib;
-use Video::Capture::zvbi;
+use Video::ZVBI qw(/^VBI_/);
 
+# default values for optional command line parameters
 $opt_duration = 0;
 $opt_device = "/dev/vbi0";
 $opt_dvbpid = undef;
@@ -33,19 +34,19 @@ $opt_debug = 0;
 ParseArgv();
 
 my $err;
-my $srv = Video::Capture::zvbi::VBI_SLICED_TELETEXT_B |
-          Video::Capture::zvbi::VBI_SLICED_VPS;
+my $srv = VBI_SLICED_TELETEXT_B | VBI_SLICED_VPS;
 my $cap;
 if (defined($opt_dvbpid)) {
-   $cap = Video::Capture::zvbi::capture::dvb_new($opt_device, 0, $srv, 0, $err, $opt_debug);
+   $cap = Video::ZVBI::capture::dvb_new($opt_device, 0, $srv, 0, $err, $opt_debug);
    $cap->dvb_filter($opt_dvbpid) 
 } else {
-   $cap = Video::Capture::zvbi::capture::v4l2_new($opt_device, 6, $srv, 0, $err, $opt_debug);
+   $cap = Video::ZVBI::capture::v4l2_new($opt_device, 6, $srv, 0, $err, $opt_debug);
 }
 
 die "Failed to open capture device: $err\n" unless $cap;
 
 $start_t = time;
+$last_pg = -1;
 $vbi_fd = $cap->fd();
 
 for(;;) {
@@ -56,19 +57,27 @@ for(;;) {
    my ($buf, $lcount, $ts);
    my $ret = $cap->pull_sliced($buf, $lcount, $ts, 0);
    if (($ret > 0) && ($lcount > 0)) {
-      my $lines = $cap->unpack_sliced($buf, $lcount, Video::Capture::zvbi::VBI_SLICED_TELETEXT_B);
-      feed(@$lines);
-
-      $lines = $cap->unpack_sliced($buf, $lcount, Video::Capture::zvbi::VBI_SLICED_VPS);
-      if ($#$lines >= 0) {
-         feed_vps($lines->[0]);
+      for (my $idx = 0; $idx < $lcount; $idx++) {
+         my @tmpl = Video::ZVBI::get_sliced_line($buf, $idx);
+         if ($tmpl[1] & VBI_SLICED_TELETEXT_B) {
+            feed($tmpl[0]);
+         } elsif ($tmpl[1] & VBI_SLICED_VPS) {
+            feed_vps($tmpl[0]);
+         }
       }
 
-      exit(0) if ($opt_duration != 0) && ((time - $start_t) > $opt_duration);
+      if (($opt_duration != 0) && ((time - $start_t) > $opt_duration)) {
+         # capture duration limit reached - done
+         exit(0);
+      }
+
+   } elsif ($ret < 0) {
+      die "capture error: $!\n";
    }
 }
 
 sub feed_vps {
+   #my $cni = Video::ZVBI::decode_vps_cni($_[0]); # only since libzvbi 0.2.22
 
    my $cd_02 = ord(substr($_[0],  2, 1));
    my $cd_08 = ord(substr($_[0],  8, 1));
@@ -90,66 +99,67 @@ sub feed_vps {
 }
 
 sub feed {
+   my ($data) = @_;
    my $lpage = -1;
-   for (@_) {
-      my $mpag = Video::Capture::zvbi::unham16p($_);
-      my $mag = $mpag & 7;
-      my $y = ($mpag & 0xf8) >> 3;
-      {
-         if ($y == 0) {
-            # teletext page header (packet #0)
-            my $page = ($mag << 8) | Video::Capture::zvbi::unham16p($_, 2);
-            my $ctrl = (Video::Capture::zvbi::unham16p($_, 4)) |
-                       (Video::Capture::zvbi::unham16p($_, 6) << 8) |
-                       (Video::Capture::zvbi::unham16p($_, 8) << 16);
-            # drop filler packets (xFF and repetition of identical pkg 0)
-            if ( (($page & 0xFF) != 0xFF) && ($page != $lpage) ) {
-               Video::Capture::zvbi::unpar_str($_);
-               $lpage = $page;
-               $sub = $ctrl & 0xffff;
-               # format: pageno, ctrl bis, ctrl bits, pkgno
-               my $buf = pack("SSCCa40", $page, ($ctrl) & 0xFFFF, ($ctrl)>>16, $y, substr($_, 2, 40));
-               syswrite(STDOUT, $buf);
-               print STDERR sprintf("%03X.%04X\n", $page, $ctrl & 0x3f7f);
-            }
-
-         } elsif($y<=25) {
-            # regular teletext packet (lines 1-25)
-            Video::Capture::zvbi::unpar_str($_);
-            my $buf = pack("SSCCa40", $mag << 8, 0, 0, $y, substr($_, 2, 40));
-            syswrite(STDOUT, $buf);
-            $lpage = -1;
-         } elsif($y==30) {
-            my $dc = (Video::Capture::zvbi::unham16p($_, 2) & 0x0F) >> 1;
-            if ($dc == 0) {
-               # packet 8/30/1
-               my $cni = Video::Capture::zvbi::rev16p($_, 2+7);
-               if (($cni != 0) && ($cni != 0xffff)) {
-                  my $buf = pack("SSCCSx18a20", $mag << 8, 0, 0, $y, $cni, substr($_, 2+20, 20));
-                  syswrite(STDOUT, $buf);
-               }
-            } elsif ($dc == 1) {
-               # packet 8/30/2
-               my $c0 = Video::Capture::zvbi::rev8(Video::Capture::zvbi::unham16p($_, 2+9+0));
-               my $c6 = Video::Capture::zvbi::rev8(Video::Capture::zvbi::unham16p($_, 2+9+6));
-               my $c8 = Video::Capture::zvbi::rev8(Video::Capture::zvbi::unham16p($_, 2+9+8));
-
-               my $cni = (($c0 & 0xf0) << 8) |
-                         (($c0 & 0x0c) << 4) |
-                         (($c6 & 0x30) << 6) |
-                         (($c6 & 0x0c) << 6) |
-                         (($c6 & 0x03) << 4) |
-                         (($c8 & 0xf0) >> 4);
-               $cni &= 0x0FFF if (($cni & 0xF000) == 0xF000);
-               if (($cni != 0) && ($cni != 0xffff)) {
-                  my $buf = pack("SSCCSx18x20", $mag << 8, 0, 0, $y, $cni);
-                  syswrite(STDOUT, $buf);
-               }
-            }
-         } else {
-            $lpage = -1;
+   my $mpag = Video::ZVBI::unham16p($data);
+   my $mag = $mpag & 7;
+   my $y = ($mpag & 0xf8) >> 3;
+   if ($y == 0) {
+      # teletext page header (packet #0)
+      my $page = ($mag << 8) | Video::ZVBI::unham16p($data, 2);
+      my $ctrl = (Video::ZVBI::unham16p($data, 4)) |
+                 (Video::ZVBI::unham16p($data, 6) << 8) |
+                 (Video::ZVBI::unham16p($data, 8) << 16);
+      # drop filler packets (xFF and repetition of identical pkg 0)
+      if ( (($page & 0xFF) != 0xFF) && ($page != $lpage) ) {
+         Video::ZVBI::unpar_str($data);
+         $lpage = $page;
+         $sub = $ctrl & 0xffff;
+         # format: pageno, ctrl bis, ctrl bits, pkgno
+         my $buf = pack("SSCCa40", $page, ($ctrl) & 0xFFFF, ($ctrl)>>16, $y, substr($data, 2, 40));
+         syswrite(STDOUT, $buf);
+         my $spg = ($page << 16) | ($ctrl & 0x3f7f);
+         if ($spg != $last_pg) {
+            print STDERR sprintf("%03X.%04X\n", $page, $ctrl & 0x3f7f);
+            $last_pg = $spg;
          }
       }
+
+   } elsif($y<=25) {
+      # regular teletext packet (lines 1-25)
+      Video::ZVBI::unpar_str($data);
+      my $buf = pack("SSCCa40", $mag << 8, 0, 0, $y, substr($data, 2, 40));
+      syswrite(STDOUT, $buf);
+      $lpage = -1;
+   } elsif($y==30) {
+      my $dc = (Video::ZVBI::unham16p($data, 2) & 0x0F) >> 1;
+      if ($dc == 0) {
+         # packet 8/30/1
+         my $cni = Video::ZVBI::rev16p($data, 2+7);
+         if (($cni != 0) && ($cni != 0xffff)) {
+            my $buf = pack("SSCCSx18a20", $mag << 8, 0, 0, $y, $cni, substr($data, 2+20, 20));
+            syswrite(STDOUT, $buf);
+         }
+      } elsif ($dc == 1) {
+         # packet 8/30/2
+         my $c0 = Video::ZVBI::rev8(Video::ZVBI::unham16p($data, 2+9+0));
+         my $c6 = Video::ZVBI::rev8(Video::ZVBI::unham16p($data, 2+9+6));
+         my $c8 = Video::ZVBI::rev8(Video::ZVBI::unham16p($data, 2+9+8));
+
+         my $cni = (($c0 & 0xf0) << 8) |
+                   (($c0 & 0x0c) << 4) |
+                   (($c6 & 0x30) << 6) |
+                   (($c6 & 0x0c) << 6) |
+                   (($c6 & 0x03) << 4) |
+                   (($c8 & 0xf0) >> 4);
+         $cni &= 0x0FFF if (($cni & 0xF000) == 0xF000);
+         if (($cni != 0) && ($cni != 0xffff)) {
+            my $buf = pack("SSCCSx18x20", $mag << 8, 0, 0, $y, $cni);
+            syswrite(STDOUT, $buf);
+         }
+      }
+   } else {
+      $lpage = -1;
    }
 }
 
